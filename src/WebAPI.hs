9884d6794cb50650c1236e6b583f1856eeec8c7b
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2016-03-10 16:06:20 -0500

    MVar for shared state

--- src/WebAPI.hs
+++ src/WebAPI.hs
@@ -5,27 +5,47 @@ module WebAPI where
 
 import LiarsPoker
 
-import Control.Lens
+import           Control.Concurrent.MVar
+import           Control.Lens
+import           Control.Monad.IO.Class
 import           Network.Wai
 import           Servant
 import           Servant.API
+import           System.IO.Unsafe (unsafePerformIO)
 
-type GameAPI = "game" :> "new-game" :> Post '[JSON] Game
+type GameAPI = "game" :> "new-game"
+                      :> QueryParams "players" String
+                      :> Post '[JSON] Integer
 
-type UserAPI = "user" :> Capture "gameId" Integer
-                      :> Capture "playerId" Int
+type PlayerAPI = "player" :> Capture "gameId" Integer
+                      :> Capture "playerIdx" Int
                       :> Get '[JSON] Player
 
-type API = GameAPI :<|> UserAPI
+type CurrentBidAPI = "current-bid" :> Capture "gameId" Integer
+                                   :> Get '[JSON] Bid
 
-server :: Server API
-server = game :<|> player
-  where
-    game = return game5
-    player gId pId = return $ game5 ^. players . singular (ix 0)
+-- type PlayAPI = "play" :>
+
+type API = GameAPI :<|> PlayerAPI :<|> CurrentBidAPI
+
+server :: MVar Game -> Server API
+server gRef = game gRef :<|> player gRef :<|> currentBid gRef
+    where
+      game gr p = do
+        let newG = newGame 42 0 ["Moe", "Larry", "Curly"]
+        liftIO $ putMVar gr newG
+        return 0
+
+      player gr gId pId = do
+        g <- liftIO $ readMVar gr
+        return $ g ^. players . singular (ix pId)
+
+      currentBid gr gId = do
+        g <- liftIO $ readMVar gr
+        return $ g ^. bid
 
 api :: Proxy API
 api = Proxy
 
 app :: Application
-app = serve api server
+app = serve api (server (unsafePerformIO newEmptyMVar))
