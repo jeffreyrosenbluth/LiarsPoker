164fbcb5258a439f951926655fd0c155265f4e34
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2016-06-30 10:34:40 -0400

    get port from environment

--- Backend/src/Main.hs
+++ Backend/src/Main.hs
@@ -7,10 +7,15 @@ import           Data.IntMap                    (empty)
 import qualified Network.Wai.Handler.Warp       as Warp
 import qualified Network.Wai.Handler.WebSockets as WaiWS
 import qualified Network.WebSockets             as WS
+import           System.Environment
 
 main :: IO ()
 main = do
+  env <- getEnvironment
+  let port = maybe 9160 read $ lookup "PORT" env
   gmRef <- newMVar empty
   Warp.runSettings
-    (Warp.setPort 9160 Warp.defaultSettings)
-    $ WaiWS.websocketsOr WS.defaultConnectionOptions (application gmRef) staticApp
+    (Warp.setPort port Warp.defaultSettings)
+    $ WaiWS.websocketsOr WS.defaultConnectionOptions
+                         (application gmRef)
+                         staticApp
